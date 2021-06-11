7f105ed24baef633f9a8f65f63a6a1a1d474d7d9
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-11 10:31:32 +0800

    Print diamond
    【问题描述】编写程序，从键盘输入一个大写字母，打印出一个菱形。该菱形中间一行由此字母组成，其相邻的上下两行由它前面的一个字母组成，按此规律，直到字母A出现在第一行和最末行为止。
    
    【样例输入1】
    D
    【样例输出1】
    
       A
      BBB
     CCCCC
    DDDDDDD
     CCCCC
      BBB
       A

---
+++ job2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class job2 {
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        //System.out.printf("Please type a capital letter: ");
+        char letter = s.next().charAt(0);
+        int line;
+        line = (int) letter - 64;
+        char aa = 64;
+
+        for (int i=1 ;i<=line;i++){
+            for(int j=1;j<=line-i;j++){
+                System.out.printf(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.printf("%c",aa+i);
+            }
+            System.out.println();
+        }
+        for (int i=line-1 ;i>=1;i--){
+            for(int j=1;j<=line-i;j++){
+                System.out.printf(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.printf("%c",aa+i);
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
+
+
